f1a077bae78ec50d9fddff2663b7c6eb9834f1aa
Cogic <2739214247@qq.com>
2022-04-22 13:40:04 +0800

    设置cookie max-age

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ const router = require('./routes')
 const app = express()
 const port = 3030
 
-process.on('uncaughtException',(err,origin)=>{
+process.on('uncaughtException', (err, origin) => {
   // FIXME 此为偷懒的做法，实则应该预见可能报错的代码，对其进行异常捕获等
   // 用于防止 node 进程因为报错而退出
-  console.log('Error ', err);
-  console.log('Origin ', origin);
+  console.log('Error ', err)
+  console.log('Origin ', origin)
 })
 
 // 用于登录的 session
@@ -19,6 +19,7 @@ app.use(
   cookieSession({
     name: 'session',
     keys: ['key1', 'key2'],
+    maxAge: 24 * 60 * 60 * 1000 * 7,
   })
 )
 
